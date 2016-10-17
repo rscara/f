37e74105ac93e073dc1d2b6e475404f306614f82
rscara <rafaedison@gmail.com>
2016-10-17 18:20:08 -0300

    no message

--- TicketInco/src/main/java/middleware/ws/service/BookQueryServiceImpl.java
+++ TicketInco/src/main/java/middleware/ws/service/BookQueryServiceImpl.java
@@ -2,16 +2,25 @@ package middleware.ws.service;
 
 import javax.jws.WebService;
 
+import middleware.business.EventManager;
 import middleware.ws.BookQueryRequest;
 import middleware.ws.BookQueryResponse;
 
 @WebService(endpointInterface = "middleware.ws.service.BookQueryService", name = "BookQueryService", targetNamespace = "http://ticketinco.com/")
 public class BookQueryServiceImpl implements BookQueryService {
+	
+	private EventManager eventManager;
 
 	@Override
-	public BookQueryResponse  queryBook(BookQueryRequest bookQueryRequest) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public BookQueryResponse queryBook(BookQueryRequest request) throws Exception {
+		int bookState = eventManager.getBookState(request.getBookId());
+		BookQueryResponse response = new BookQueryResponse();
+		response.setBookState(bookState);
+		return response;
+	}
+
+	public void setEventManager(EventManager eventManager) {
+		this.eventManager = eventManager;
 	}
 
 }
--- TicketInco/src/main/java/middleware/ws/service/TicketBookServiceImpl.java
+++ TicketInco/src/main/java/middleware/ws/service/TicketBookServiceImpl.java
@@ -2,16 +2,25 @@ package middleware.ws.service;
 
 import javax.jws.WebService;
 
+import middleware.business.EventManager;
 import middleware.ws.TicketBookRequest;
 import middleware.ws.TicketBookResponse;
 
 @WebService(endpointInterface = "middleware.ws.service.TicketBookService", name = "TicketBookService", targetNamespace = "http://ticketinco.com/")
 public class TicketBookServiceImpl implements TicketBookService {
+	
+	private EventManager eventManager;
 
 	@Override
-	public TicketBookResponse bookEvent(TicketBookRequest ticketBookRequest) {
-		// TODO Auto-generated method stub
-		return null;
+	public TicketBookResponse bookEvent(TicketBookRequest request) throws Exception {
+		long bookId = eventManager.bookTickets(request.getEventId(), request.getEventDate(), request.getEventSchedules());
+		TicketBookResponse response = new TicketBookResponse();
+		response.setBookId(bookId);
+		return response;
+	}
+
+	public void setEventManager(EventManager eventManager) {
+		this.eventManager = eventManager;
 	}
 
 }
