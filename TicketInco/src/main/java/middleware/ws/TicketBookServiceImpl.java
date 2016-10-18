e0514ac00505c5e24276ac156551124843f9b571
javierhagopian <jhagopian@gmail.com>
2016-10-18 10:13:14 -0300

    ddd

--- TicketInco/src/main/java/middleware/ws/TicketBookServiceImpl.java
+++ TicketInco/src/main/java/middleware/ws/TicketBookServiceImpl.java
@@ -2,18 +2,23 @@ package middleware.ws;
 
 import javax.jws.WebService;
 
+import org.apache.log4j.Logger;
+
 import middleware.business.EventManager;
 
 @WebService(endpointInterface = "middleware.ws.TicketBookService", name = "TicketBookService", targetNamespace = "http://ticketinco.com/")
 public class TicketBookServiceImpl implements TicketBookService {
 	
+	private static final Logger logger = Logger.getLogger(TicketBookServiceImpl.class);
 	private EventManager eventManager;
 
 	@Override
 	public TicketBookResponse bookEvent(TicketBookRequest request) throws Exception {
+		logger.debug("Request:" + request.toString());
 		long bookId = eventManager.bookTickets(request.getEventId(), request.getEventDate(), request.getEventSchedules());
 		TicketBookResponse response = new TicketBookResponse();
 		response.setBookId(bookId);
+		logger.debug("Response:" + response.toString());
 		return response;
 	}
 
--- TicketInco/src/main/java/middleware/ws/TicketQueryServiceImpl.java
+++ TicketInco/src/main/java/middleware/ws/TicketQueryServiceImpl.java
@@ -1,20 +1,23 @@
 package middleware.ws;
 
 import javax.jws.WebService;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-import middleware.DateAdapter;
+import org.apache.log4j.Logger;
+
 import middleware.business.EventManager;
 
 @WebService(endpointInterface = "middleware.ws.TicketQueryService", name = "TicketQueryService", targetNamespace = "http://ticketinco.com/")
 public class TicketQueryServiceImpl implements TicketQueryService {
 	
+	private static final Logger logger = Logger.getLogger(TicketQueryServiceImpl.class);
 	private EventManager eventManager;
 
 	@Override
 	public TicketQueryResponse queryTickets(TicketQueryRequest request) {
+		logger.debug("Request:" + request.toString());
 		TicketQueryResponse response = new TicketQueryResponse();
 		response.setEvent(eventManager.getScheduleForEvent(request.getEventID(), request.getEventDate()));
+		logger.debug("Response:" + response.toString());
 		return response;
 	}
 
