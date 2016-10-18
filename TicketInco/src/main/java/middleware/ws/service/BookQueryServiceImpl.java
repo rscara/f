af24386180a27b8f380f4bcc35c7d1cd5e7dacd0
javierhagopian <jhagopian@gmail.com>
2016-10-18 17:34:37 -0300

    yyy

--- TicketInco/src/main/java/middleware/ws/service/BookQueryServiceImpl.java
+++ TicketInco/src/main/java/middleware/ws/service/BookQueryServiceImpl.java
@@ -2,16 +2,31 @@ package middleware.ws.service;
 
 import javax.jws.WebService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import middleware.business.EventManager;
 import middleware.ws.BookQueryRequest;
 import middleware.ws.BookQueryResponse;
 
 @WebService(endpointInterface = "middleware.ws.service.BookQueryService", name = "BookQueryService", targetNamespace = "http://ticketinco.com/")
 public class BookQueryServiceImpl implements BookQueryService {
+	
+	private static final Logger logger = LogManager.getLogger(BookQueryServiceImpl.class);
+	private EventManager eventManager;
 
 	@Override
-	public BookQueryResponse  queryBook(BookQueryRequest bookQueryRequest) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public BookQueryResponse queryBook(BookQueryRequest request) throws Exception {
+		logger.debug("Request:" + request.toString());
+		int bookState = eventManager.getBookState(request.getBookId());
+		BookQueryResponse response = new BookQueryResponse();
+		response.setBookState(bookState);
+		logger.debug("Response:" + response.toString());
+		return response;
+	}
+
+	public void setEventManager(EventManager eventManager) {
+		this.eventManager = eventManager;
 	}
 
 }
--- TicketInco/src/main/java/middleware/ws/service/TicketBookServiceImpl.java
+++ TicketInco/src/main/java/middleware/ws/service/TicketBookServiceImpl.java
@@ -2,16 +2,31 @@ package middleware.ws.service;
 
 import javax.jws.WebService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import middleware.business.EventManager;
 import middleware.ws.TicketBookRequest;
 import middleware.ws.TicketBookResponse;
 
 @WebService(endpointInterface = "middleware.ws.service.TicketBookService", name = "TicketBookService", targetNamespace = "http://ticketinco.com/")
 public class TicketBookServiceImpl implements TicketBookService {
+	
+	private static final Logger logger = LogManager.getLogger(TicketBookServiceImpl.class); 
+	private EventManager eventManager;
 
 	@Override
-	public TicketBookResponse bookEvent(TicketBookRequest ticketBookRequest) {
-		// TODO Auto-generated method stub
-		return null;
+	public TicketBookResponse bookEvent(TicketBookRequest request) throws Exception {
+		logger.debug("Request:" + request.toString());
+		long bookId = eventManager.bookTickets(request.getEventId(), request.getEventDate(), request.getEventSchedules());
+		TicketBookResponse response = new TicketBookResponse();
+		response.setBookId(bookId);
+		logger.debug("Response:" + response.toString());
+		return response;
 	}
 
+	public void setEventManager(EventManager eventManager) {
+		this.eventManager = eventManager;
+	}
 }
+
